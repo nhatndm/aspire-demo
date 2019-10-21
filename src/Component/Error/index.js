eb0f327c1f2b50d908c2526bc8554803ed046571
nhatndm <nhatndm1193@gmail.com>
2019-10-21 16:04:23 +0700

    fixed bug class

--- src/Component/Error/index.js
+++ src/Component/Error/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const ErrorLabel = ({ mess }) => {
   if (mess) {
     return (
-      <div class="alert alert-danger" role="alert">
+      <div className="alert alert-danger" role="alert">
         {mess}
       </div>
     );
